c32a0aa1f35454ddba966f3b9b1c7ef3b8253fd3
feketrob <rofekete@gmail.com>
2017-08-17 14:57:22 +0200

    Solving second part of Day 22

--- 22/main.cpp
+++ 22/main.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <vector>
 #include <functional>
+#include <queue>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ int main(int argc, char* argv[])
 		cout << solve_first(f) << endl;
 		f.clear();
 		f.seekg(0, ios::beg);
-		// cout << solve_second(f) << endl;
+		cout << solve_second(f) << endl;
 	}
 	else{
 		cout << "File not found" << endl;
@@ -86,7 +87,6 @@ map<pair<int, int>, pair<int, int>> parse(istream& input)
 int solve_first(istream& input){
 
   auto disks = parse(input);
-  auto dimension = (int)sqrt(disks.size());
   vector<pair<int, int>> values;
   for (auto disk : disks)
   {
@@ -116,7 +116,162 @@ int solve_first(istream& input){
 	return result;
 }
 
+pair<int, int> get_dimension(map<pair<int, int>, pair<int, int>>& disks)
+{
+  int width = 0;
+  int height = 0;
+  for (auto disk : disks)
+  {
+    if (disk.first.first > width)
+    {
+      width = disk.first.first;
+    }
+    if (disk.first.second > height)
+    {
+      height = disk.first.second;
+    }
+  }
+
+  return make_pair(width + 1, height + 1);
+}
+
+vector<pair<int, int>> get_neightbours(pair<int, int>& node, map<pair<int, int>, pair<int, int>>& disks)
+{
+  pair<int, int> indices[4]{ make_pair(1, 0),  make_pair(0, 1), make_pair(0, -1), make_pair(-1, 0) };
+  vector<pair<int, int>> neighbours;
+
+  auto dimension = get_dimension(disks);
+
+  for (auto index : indices)
+  {
+    auto new_index = make_pair(node.first + index.first, node.second + index.second);
+    if (new_index.first < 0 || new_index.first >= dimension.first || new_index.second < 0 || new_index.second >= dimension.second)
+    {
+      continue;
+    }
+    neighbours.push_back(new_index);
+  }
+  return neighbours;
+}
+
+vector<pair<pair<int, int>, pair<int, int>>> bfs(pair<int, int>& source, pair<int, int>& target, map<pair<int, int>, pair<int, int>>& disks, function<bool(pair<int, int>, pair<int, int>)> pred, pair<int, int>& excluded)
+{
+  vector<pair<pair<int, int>, pair<int, int>>> path;
+  queue<pair<int, int>> q;
+  q.push(source);
+  vector<pair<int, int>> visited;
+  map<pair<int, int>, pair<int, int>> from_list;
+
+  while (q.size() > 0)
+  {
+    auto node = q.front();
+    q.pop();
+    if (find(begin(visited), end(visited), node) != end(visited) || node == excluded)
+    {
+      continue;
+    }
+    visited.push_back(node);
+
+    if (node == target)
+    {
+      auto temp = node;
+      auto prev = temp;
+      while (temp != source)
+      {
+        temp = from_list[prev];
+        path.push_back(make_pair(temp, prev));
+        prev = temp;
+      }
+      reverse(begin(path), end(path));
+
+      return path;
+    }
+
+    auto neighbours = get_neightbours(node, disks);
+    for (auto next : neighbours)
+    {
+      if (pred(disks[node], disks[next]))
+      {
+        continue;
+      }
+
+      if (from_list.find(next) == end(from_list))
+      {
+        from_list[next] = node;
+      }
+      q.push(next);
+    }
+  }
+  return path;
+}
+
+vector<pair<pair<int, int>, pair<int, int>>> bfs(pair<int, int>& source, pair<int, int>& target, map<pair<int, int>, pair<int, int>>& disks, function<bool(pair<int, int>, pair<int, int>)> pred)
+{
+  return bfs(source, target, disks, pred, make_pair(-1, -1));
+}
+
+void copy_data(pair<int, int> from, pair<int, int> to, map < pair<int, int>, pair<int, int>>& disks)
+{
+  auto copied = disks[from].first;
+  disks[to].first += copied;
+  disks[to].second -= copied;
+  disks[from].first -= copied;
+  disks[from].second += copied;
+}
+
+int make_it_empty(pair<int, int>& node, map<pair<int, int>, pair<int, int>>& disks, pair<int, int>& excluded)
+{
+  pair<int, int> empty_spot;
+  for (auto disk : disks)
+  {
+    if (disk.second.first == 0)
+    {
+      empty_spot = disk.first;
+      break;
+    }
+  }
+  auto path = bfs(empty_spot, node, disks, [](pair<int, int> to, pair<int, int>from)
+  {
+    return from.first > (to.first + to.second);
+    //return to.second < from.first;
+  }, excluded);
+
+  for (auto edge : path)
+  {
+    copy_data(edge.second, edge.first, disks);
+  }
+
+  return path.size();
+}
+
 int solve_second(istream& input){
-	return 0;
+
+  auto disks = parse(input);
+  auto dimension = get_dimension(disks);
+  vector<pair<int, int>> values;
+  auto info = make_pair(dimension.first - 1, 0);
+
+
+  for (auto disk : disks)
+  {
+    values.push_back(disk.second);
+  }
+  
+  auto result = 0;
+  auto path = bfs(info, make_pair(0, 0), disks, [](pair<int, int> a, pair<int, int>b)
+  {
+    return a.first > (b.first + b.second);
+  });
+
+  for (auto edge : path)
+  {
+    auto next = edge.second;
+    result += make_it_empty(next, disks, info);
+    copy_data(info, next, disks);
+    info = next;
+    result++;
+  }
+
+  return result;
 }
 
